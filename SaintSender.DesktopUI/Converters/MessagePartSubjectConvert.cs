80fdb0cd4d96aee162d377db3db5228523d3e970
Daniel Zsk <dzsukov@gmail.com>
2019-12-18 14:30:48 +0100

    Istu Commit: Converter implemented

---
+++ SaintSender.DesktopUI/Converters/MessagePartSubjectConvert.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace SaintSender.DesktopUI.Converters
+{
+    class MessagePartSubjectConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var msgPart = (MessagePart)value;
+            var allHeaders = msgPart.Headers;
+
+
+            foreach (var item in allHeaders)
+            {
+                if (item.Name == "Subject")
+                {
+                    return item.Value;
+                }
+            }
+
+            return null;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
