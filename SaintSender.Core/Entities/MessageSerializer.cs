0bb2d7249b0199407995f9b36f196d9c09f86235
Daniel Zsk <dzsukov@gmail.com>
2019-12-19 10:43:41 +0100

    Serialization changed into static methods, JSON serializer used, WPF Frontend created to facilitate Saving, Command Bindings created in Async

---
+++ SaintSender.Core/Entities/MessageSerializer.cs
@@ -0,0 +1,38 @@
+﻿using Google.Apis.Gmail.v1.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.Core.Entities
+{
+
+    public class MessageSerializer
+    {
+
+        public static void SerializeMessage(Message originalMsg)
+        {
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            using (StreamWriter txtwriter = File.CreateText($"C:\\Users\\Main\\Desktop\\Codecool_Advanced\\tw_3\\SaintSender\\SaintSender.Core\\SavedMessages\\{originalMsg.Id}.dat"))
+            {
+                jsonSerializer.Serialize(txtwriter, originalMsg);
+            }
+        }
+
+        public static Message Deserialize(string msgId)
+        {
+            Message msg = new Message();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (Stream fs = File.Open($"C:\\Users\\Main\\Desktop\\Codecool_Advanced\\tw_3\\Serialization\\Serialisation\\SavedObjects\\{msgId}.dat", FileMode.Open))
+            {
+                msg = (Message)binaryFormatter.Deserialize(fs);
+            }
+            return msg;
+        }
+    }
+}
--- SaintSender.DesktopUI/ViewModels/AllMailViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/AllMailViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Google.Apis.Gmail.v1.Data;
 using SaintSender.Core.Entities;
 using SaintSender.Core.Services;
+using SaintSender.DesktopUI.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,11 +9,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace SaintSender.DesktopUI.ViewModels
 {
     public class AllMailViewModel
     {
+        public ICommand SaveEmails { get; private set; }
         public ObservableCollection<Message> Mails { get; private set; }
         public IList<Label> Folders { get; private set; }
         public GreetService Service { get; private set; }
@@ -23,11 +26,16 @@ namespace SaintSender.DesktopUI.ViewModels
             Service = new GreetService();
             Folders = ShowFolders();
             Mails = new ObservableCollection<Message>();
-            Task getMails = Task.Factory.StartNew(()=>ShowMails());
-
+            Task getMails = Task.Factory.StartNew(() => ShowMails());
             BindingOperations.EnableCollectionSynchronization(Mails, _itemsLock);
+            SaveEmails = new Commands(
+                (obj) => { return true; },
+                (obj) => Mails.ToList().ForEach((item) => SaveMails(item))
+                );
+        }
 
-        }    
+
+            
 
 
         public IList<Label> ShowFolders()
@@ -38,12 +46,12 @@ namespace SaintSender.DesktopUI.ViewModels
         public void ShowMails()
         {
             Service.GetMails("INBOX", Mails, _itemsLock);
-            foreach (var item in Mails)
-            {
-                SerializableMessage msg = new SerializableMessage(item);
-                msg.SerializeMessage();
-            }
-
         } 
+
+        public async void SaveMails(Message item)
+        {
+            await Task.Run(()=>MessageSerializer.SerializeMessage(item));
+        }
+        
+        }
     }
-}
