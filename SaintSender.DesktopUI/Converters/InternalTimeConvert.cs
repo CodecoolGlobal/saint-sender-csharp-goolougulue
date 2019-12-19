f3ea369f77686fa1e0c7a5b399d8f3c294d42004
leinwanderIstvan <coolcodebasic@gmail.com>
2019-12-19 13:11:03 +0100

    dateTime converter implemented

---
+++ SaintSender.DesktopUI/Converters/InternalTimeConvert.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Gmail.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SaintSender.DesktopUI.Converters
+{
+    public class InternalTimeConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var dateTime = ((long)value);
+            
+
+            var convertTime = DateTimeOffset.FromUnixTimeMilliseconds(dateTime).DateTime;
+            return TimeZoneInfo.ConvertTimeToUtc(convertTime);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
