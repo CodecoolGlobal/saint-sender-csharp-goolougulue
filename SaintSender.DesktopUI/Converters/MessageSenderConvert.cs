06f7d5f4571e2c4bebe76ca6aed5e8a6994aa2c6
leinwanderIstvan <coolcodebasic@gmail.com>
2019-12-19 13:46:47 +0100

    message_sender_class implemented and binding to view

---
+++ SaintSender.DesktopUI/Converters/MessageSenderConvert.cs
@@ -0,0 +1,31 @@
+﻿using Google.Apis.Gmail.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SaintSender.DesktopUI.Converters
+{
+    class MessageSenderConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var msgPart = (MessagePart)value;
+            var fromHeader = msgPart.Headers;
+
+            var item = fromHeader.FirstOrDefault(header => header.Name == "From" || header.Name == "from");
+
+            return item.Value;
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
